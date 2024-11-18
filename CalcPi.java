562d2ca73b437a4d02e7ae31122284c43725b7a5
Agam Yerucham <agamyerucham@Agams-MacBook-Pro.local>
2024-11-18 15:37:02 +0200

    finished 2nd task

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,27 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+	  int numberOfTerms = Integer.parseInt(args [0] );
+	  double approximatedPi = 0.0;
+	  for (int i = 0 ; i< numberOfTerms ; i++ )
+	  {
+		Double calcuation = ( 1.0 / (2 * i + 1) ) ; 
+		/*
+		 * כדי למצוא את המיקום - צריך לבדוק חוקיות מתי מוסיפים ומתי מחסירים את האיבר
+		 *   לפי הדוגמא איבר במקום האפס אפשר מקבלים אותו כפלוס אבל במקום האחד 
+		 *  2 מקבלים במינוס וכן הלאה בכפולות של 2 ולכן אני יכולה להשתמש במודלו
+		 * כדי להבין איזה פעולה לשים מתי
+		 */
+		
+		if ( i % 2 == 0 )
+		{
+			approximatedPi += calcuation;
+		} else {
+			approximatedPi -= calcuation;
+		}
+	  } 
+	  System.out.println("pi according to Java: " + Math.PI);
+	  System.out.println("pi, approximated: " + 4 * (approximatedPi));
 }
+}
+	
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,15 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
-}
+     int hours = Integer.parseInt( args[0] ); //22 , 23
+     int minutes = Integer.parseInt( args[1] ) ; //07 , 59
+     int minuteToAdd = Integer.parseInt( args[2] ) ; //12345 , 10
+     int totalMinutes = ( hours * 60 ) + minutes +minuteToAdd ; // 22*60 + 07 +12345 = 13672 ,23*60 +59 +10 =1449
+     int totalHours = totalMinutes / 60 ; //227 , 24
+     int newHours = totalHours % 24 ; // 11 , 0
+     int newMinutes = totalMinutes - ( totalHours * 60 ) ; //13672 - (227*60) = 52 ,1449 - (24*60) = 9 
+     System.out.println( "New hours: " + newHours ) ;
+     System.out.println( "New minutes: " + newMinutes ); 
+    } 
+ }
+ 
+
