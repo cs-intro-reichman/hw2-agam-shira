8b96d57406f947ab48ae8df9d8b753a75453325b
Agam Yerucham <agamyerucham@Agams-MacBook-Pro.local>
2024-11-18 22:20:06 +0200

    finished 2nd task

--- TimeCalc.java
+++ TimeCalc.java
@@ -3,13 +3,23 @@ public class TimeCalc {
      String hoursMinutes = args[0]; 
      int hours = Integer.parseInt(""+hoursMinutes.charAt(0) + hoursMinutes.charAt(1));
      int minutes = Integer.parseInt(""+hoursMinutes.charAt(3) + hoursMinutes.charAt(4));
-     int minuteToAdd = Integer.parseInt( args[3] ) ; //12345 , 10
+     int minuteToAdd = Integer.parseInt( args[1] ) ; //12345 , 10
      int totalMinutes = ( hours * 60 ) + minutes +minuteToAdd ; // 22*60 + 07 +12345 = 13672 ,23*60 +59 +10 =1449
      int totalHours = totalMinutes / 60 ; //227 , 24
      int newHours = totalHours % 24 ; // 11 , 0
      int newMinutes = totalMinutes - ( totalHours * 60 ) ; //13672 - (227*60) = 52 ,1449 - (24*60) = 9 
-     System.out.println( "New hours: " + newHours ) ;
-     System.out.println( "New minutes: " + newMinutes ); 
+     if (newHours < 10) {
+        if (newMinutes < 10) {
+            System.out.println("0" + newHours + ":" + "0" + newMinutes);
+        } else {
+            System.out.println("0" + newHours + ":" + newMinutes);
+        }
+     } else {
+        if (newMinutes < 10) { 
+            System.out.println(newHours + ":" + "0" + newMinutes);
+     } else {
+        System.out.println(newHours + ":" + newMinutes);
+     }
     } 
- }
- 
+  }
+}
