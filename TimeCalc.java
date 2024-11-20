acb1985e46116bf4b50cc7078022d7096b2f26ca
Agam Yerucham <agamyerucham@Agams-MacBook-Pro.local>
2024-11-20 22:35:43 +0200

    finished 2nd task

--- TimeCalc.java
+++ TimeCalc.java
@@ -8,7 +8,7 @@ public class TimeCalc {
      int totalHours = totalMinutes / 60 ; //227 , 24
      int newHours = totalHours % 24 ; // 11 , 0
      int newMinutes = totalMinutes - ( totalHours * 60 ) ; //13672 - (227*60) = 52 ,1449 - (24*60) = 9 
-     if (newHours < 10) {
+     if (newHours < 10){
         if (newMinutes < 10) {
             System.out.println("0" + newHours + ":" + "0" + newMinutes);
         } else {
